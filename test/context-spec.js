bdb021fddf59931615a026aca9163353bbec49c4
Everton Yoshitani <everton@notreve.com>
2018-03-16 08:14:59 +0900

    Updates to comply with eslint rules

--- test/context-spec.js
+++ test/context-spec.js
@@ -3,48 +3,31 @@
 const context = require('../lib/context')
 
 describe('context', () => {
-
   it('should fail with an empty context object', () => {
-
     let ctx = {}
 
     return context(ctx).should.be.rejectedWith('Invalid and/or missing context')
-
   })
 
   it('should fail with a number context', () => {
-
     let ctx = 0
 
     return context(ctx).should.be.rejectedWith('Invalid and/or missing context')
-
-
   })
 
   it('should fail with a string context', () => {
-
     let ctx = 'invalid'
 
     return context(ctx).should.be.rejectedWith('Invalid and/or missing context')
-
-
   })
 
   it('should fail with a array context', () => {
-
     let ctx = []
 
     return context(ctx).should.be.rejectedWith('Invalid and/or missing context')
-
-
   })
 
   it('should fail with an undefined context', () => {
-
-    let ctx = undefined
-
-    return context(ctx).should.be.rejectedWith('Invalid and/or missing context')
-
+    return context().should.be.rejectedWith('Invalid and/or missing context')
   })
-
 })
