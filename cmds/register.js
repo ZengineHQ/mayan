91a30c41b72b15e6b4f8a330e516d3535cacde8e
Alex Weber <alexweber15@gmail.com>
2018-08-02 14:48:01 -0300

    chore: wip

--- cmds/register.js
+++ cmds/register.js
@@ -7,15 +7,10 @@ exports.command = 'register [plugin]'
 exports.desc = 'Register plugin in Zengine API'
 
 exports.builder = yargs => {
-  yargs.option('dev', {
-    describe: 'Only register a dev version of the plugin',
-    type: 'boolean',
-    default: false
-  })
-  yargs.option('prod', {
-    describe: 'Only register a prod version of the plugin',
-    type: 'boolean',
-    default: false
+  yargs.positional('plugin', {
+    describe: 'Plugin name',
+    type: 'string',
+    default: '*'
   })
   yargs.option('publish', {
     describe: 'Publish the plugin after registering it',
