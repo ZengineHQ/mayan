ddef9fe9e8b23ab21be1aa66912cdcd2068b7b42
Everton Yoshitani <everton@notreve.com>
2019-01-28 04:49:32 +0900

    chore: suppress cli options on error, fail with generic message vs exception

--- cli.js
+++ cli.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const fs = require('fs')
 const yargs = require('yargs')
+const kleur = require('kleur')
 
 function parseConfig (configPath) {
   return JSON.parse(fs.readFileSync(configPath, 'utf-8'))
@@ -14,6 +15,12 @@ updateNotifier({
   updateCheckInterval: 1000 * 60 * 60 * 8 // 8 hours
 }).notify({ isGlobal: true })
 
+function onFail(msg, err) {
+  console.log()
+  console.log(kleur.red('Failed, please fix the issues above. Use --help for available options.'))
+  console.log()
+}
+
 const argv = yargs
   .commandDir('cmds')
   .option('env', { description: 'Environment name', requiresArg: true, alias: 'e' })
@@ -21,10 +28,12 @@ const argv = yargs
   .config('config', parseConfig)
   .demandCommand(1, '')
   .strict()
+  .showHelpOnFail(false)
   .help()
   // Keep things simple unless advanced users use --show-hidden
   .hide('config')
   .hide('version')
   .showHidden('show-hidden', 'Show advanced options')
   .epilogue('For more information, RTFM at https://github.com/ZengineHQ/mayan')
+  .fail(onFail)
   .argv
